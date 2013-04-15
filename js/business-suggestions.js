c5cb11149a4f2ab08d9d8fb1e4f26f1a3f57d4bc
Gil O'Neil <goneil@mit.edu>
2013-04-15 00:55:52 -0400

    got most of the suggestions page done

--- js/business-suggestions.js
+++ js/business-suggestions.js
@@ -37,6 +37,7 @@ $(document).ready(function() {
 
     var index = 0;
     var numSuggestions = 5;
+    addMode = false; // should be global
 
     $("#btnNext").click(function(){
         index = nextFunc(businessList, index, numSuggestions);
@@ -48,20 +49,60 @@ $(document).ready(function() {
     $("#btnContact").click(function(){
         if ($("#messageRow").length === 0){
             $(".container").append('<div id="messageRow" class="row maximizedMessage"  >\
-                <div class="span3" id="messageHeader">\
+                <div class="span4" id="messageHeader">\
                     <div class="row">\
-                        <div class="span2">\
-                            MessageTitle\
+                        <div class="span3 recipients">\
+                                    To:\
+                                    <input id="recipients" type="text" placeholder="recipients...">\
+                                    <i class="icon-plus-sign icon-white icon" id="btnAdd"></i>\
                         </div>\
                         <div class="span1" id="icons">\
-                            <i class="icon-plus icon-white" id="btnMaximize"></i>\
-                            <i class="icon-minus icon-white" id="btnMinimize"></i>\
-                            <i class="icon-remove icon-white" id="btnRemove"></i>\
+                            <i class="icon-plus icon-white icon" id="btnMaximize"></i>\
+                            <i class="icon-minus icon-white icon" id="btnMinimize"></i>\
+                            <i class="icon-remove icon-white icon" id="btnRemove"></i>\
                         </div>\
                     </div>\
                 </div>\
                 <div id="messageBody">\
-                    lsdkjflsdkjf\
+                    <form class="form-horizontal">\
+                      <div class="control-group">\
+                        </br>\
+                        <label class="control-label" for="theme">Theme</label>\
+                        <div class="controls">\
+                          <input type="text" id="theme" placeholder="Theme">\
+                        </div>\
+                      </div>\
+                      <div class="control-group">\
+                        <label class="control-label" for="charity">Charity</label>\
+                        <div class="controls">\
+                          <input type="text" id="charity" placeholder="Charity">\
+                        </div>\
+                      </div>\
+                      <div class="control-group">\
+                        <label class="control-label" for="date">Date</label>\
+                        <div class="controls">\
+                          <input type="text" id="date" placeholder="xx/xx/xxxx">\
+                        </div>\
+                      </div>\
+                      <div class="control-group">\
+                        <label class="control-label" for="size">Size</label>\
+                        <div class="controls">\
+                          <input type="text" id="size" placeholder="Size">\
+                        </div>\
+                      </div>\
+                      <div class="control-group">\
+                        <label class="control-label" for="messageToBusiness">Message To Business (recommended)</label>\
+                        <i class="icon-question-sign icon" id="btnHelp"></i>\
+                        <div class="controls">\
+                            <textarea rows="5"></textarea>\
+                        </div>\
+                      </div>\
+                      <div class="control-group">\
+                        <div class="controls">\
+                          <button type="button" id="btnSend" class="btn btn-primary">Send</button>\
+                        </div>\
+                      </div>\
+                    </form>\
                 </div>\
             </div>\
             ');
@@ -83,9 +124,41 @@ $(document).ready(function() {
                 console.log("remove clicked");
                 $("#messageRow").remove();
             });
+            $("#btnAdd").click(function(){
+                addMode = !addMode;
+                if (addMode){
+                    $("#btnMinimize").click();
+
+                    if ($("suggestionRow").attr("data-original-title")!==""){
+                        $("#suggestionRow").popover(
+                            {placement: "top",
+                             trigger: "manual",
+                             content:"Click business icons to add them as recipients to your message. Click the add button again to stop adding businesses."
+                            }
+                        ).popover("show");
+                    }                    
+                    $(".suggestion").effect("pulsate", {times:3}, 1500);
+                }else{
+                    $("#suggestionRow").popover("toggle");
+                }
+            });
+            $("#btnSend").click(function(){
+                addMode = false;
+                $("#messageRow").remove();
+                alert("Send not implemented");
+            });
+            $("#btnHelp").click(function(){
+                var title = "Have you answered these questions?";
+                var content = "Describe your charity...\n\nDescribe the event...\n\n" +
+                              "Explain the logistics...\n\nExplain how you'll use the donations...";
+                if ($(this).attr("data-original-title")!==""){
+                    $(this).popover({title: title, content: content}).popover("show");
+                }
+            });
         } else{
             $("#btnMaximize").click();
         }
+        $("#recipients").val($("#businessName").text());
             
     });
 
@@ -127,16 +200,20 @@ var setSuggestions = function(businessList, index, numSuggestions){
 
     $(".imageThumbnail").click(function(){
         var business = businessList[$(this).attr("index")];
-        var img = $("<img>");
-        img.attr("src", $(this).attr("src"));
-        img.width("100%");
-        img.height("100%");
+        if (addMode === false){
+            var img = $("<img>");
+            img.attr("src", $(this).attr("src"));
+            img.width("100%");
+            img.height("100%");
 
-        $("#businessIcon").html(img);
-        $("#businessName").html(business.name);
-        var keys = Object.keys(business.info);
-        for (var i = 0; i < keys.length; i ++){
-            $("#business" + keys[i]).html(business.info[keys[i]]);
+            $("#businessIcon").html(img);
+            $("#businessName").html(business.name);
+            var keys = Object.keys(business.info);
+            for (var i = 0; i < keys.length; i ++){
+                $("#business" + keys[i]).html(business.info[keys[i]]);
+            }
+        } else{
+            $("#recipients").val($("#recipients").val() + ", " + business.name);
         }
     });
 };
