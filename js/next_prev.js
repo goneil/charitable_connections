5c40ea574a7df6f09de4bb1f783dc32d95d857ff
Gil O'Neil <goneil@mit.edu>
2013-04-16 14:51:12 -0400

    added progress html and js

---
+++ js/next_prev.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    var highlightedButton = 0; // keeps track of which button is highlighted
+    $("#btnFirst").click(function(){
+        highlightedButton = 0;
+        highlightButton(highlightedButton);
+        $("#btnPrev").addClass("disabled");
+        //TODO implement function
+    });
+    $("#btnSecond").click(function(){
+        highlightedButton = 1;
+        highlightButton(highlightedButton);
+        $("#btnPrev").removeClass("disabled");
+        //TODO implement function
+    });
+    $("#btnThird").click(function(){
+        highlightedButton = 2;
+        highlightButton(highlightedButton);
+        $("#btnPrev").removeClass("disabled");
+        //TODO implement function
+    });
+    $("#btnFourth").click(function(){
+        highlightedButton = 3;
+        highlightButton(highlightedButton);
+        $("#btnPrev").removeClass("disabled");
+        //TODO implement function
+    });
+    $("#btnPrev").click(function(){
+        highlightedButton --;
+        highlightButton(highlightedButton);
+        //TODO implement functionality
+    });
+    $("#btnNext").click(function(){
+        highlightedButton ++;
+        highlightButton(highlightedButton);
+        //TODO implement functionality
+    });
+
+    $("#btnPrev").addClass("disabled");
+});
+
+// highlilghts button indexed by number.  Also takes care of previous and next
+// disabling/text changing
+var highlightButton = function(num){
+    var progressBar = $("#progressBar");
+    var children = progressBar.children();
+    console.log(children.length);
+    num = ((num % children.length) + children.length) % children.length;
+    console.log(num);
+
+    // move progressindicator
+    for (var i = 0; i < children.length; i ++){
+        if (i === num){
+            $(children[i]).addClass("btn-warning").addClass("active");
+        }else{
+            $(children[i]).removeClass("btn-warning").removeClass("active");
+        }
+    }
+
+    // enable or disable prev button
+    if (num === 0){
+        $("#btnPrev").addClass("disabled");
+    }else{
+        $("#btnPrev").removeClass("disabled");
+    }
+
+    // change text of next button to finish at end
+    if (num === 3){
+        $("#btnNext").html("Finish");
+    }else{
+        $("#btnNext").html("Next &rarr;");
+    }
+};
