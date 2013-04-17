22f66e576a4e124566b19a5c4b60f4d29dc48685
Emily Kuo <ekuo8@mit.edu>
2013-04-17 13:46:44 -0400

    added event/donation images, added repopulation of choices

--- js/create.js
+++ js/create.js
@@ -1,4 +1,4 @@
-function squarifyMe(element,ratio) {
+function squarifyRatio(element,ratio) {
     squareItUp();
     window.onresize = function(element) {
         squareItUp();
@@ -10,103 +10,108 @@ function squarifyMe(element,ratio) {
 }
 
 var charities = [
-	{ label: "American Cancer Society", image: "img/acs.png" },
-	{ label: "American Red Cross", image: "img/red_cross.png"},
-	{ label: "Green Laces", image: "img/green_laces.png" },
-	{ label: "Jimmy Fund", image: "img/jimmy_fund.png"},
-	{ label: "Partners in Health", image: "img/partners.png" },
-	{ label: "USO", image: "img/uso.png"}
+    { label: "American Cancer Society", image: "img/acs.png" },
+    { label: "American Red Cross", image: "img/red_cross.png"},
+    { label: "Green Laces", image: "img/green_laces.png" },
+    { label: "Jimmy Fund", image: "img/jimmy_fund.png"},
+    { label: "Partners in Health", image: "img/partners.png" },
+    { label: "USO", image: "img/uso.png"}
 ];
 
-var charity_list = [];
+var event_types = [
+    { label: "Automotive", image: "img/car.png" },
+    { label: "Basketball", image: "img/bball.png" },
+    { label: "Cooking", image: "img/cooking.png" },
+    { label: "Education", image: "img/edu.png" },
+    { label: "Reading", image: "img/reading.png" },
+    { label: "Soccer", image: "img/soccer.png" }
+];
+
+var donation_types = [
+    { label: "Beverages", image: "img/drinks.png" },
+    { label: "Clothes", image: "img/clothes.png" },
+    { label: "Food", image: "img/food.png" },
+    { label: "General", image: "img/general.png" },
+    { label: "Gift Cards", image: "img/gift_cards.png" },
+    { label: "Monetary", image: "img/money.png"}
+];
 
 
 function makeChoiceList(list){
-	for(var i = 0; i < list.length; i++){
-		var img = new Image();
-		img.src = list[i].image;
-		var caption = $('<div/>')
-			.html(list[i].label)
-			.addClass("choice_label");
-		var li = $('<li/>')
-			.addClass("ui-state-default")
-			.append(img)
-			.append(caption);
-		$("#choicesList").append(li);
-	}
+    $("#choicesList").empty();
+    for(var i = 0; i < list.length; i++){
+        var img = new Image();
+        img.src = list[i].image;
+        var caption = $('<div/>')
+            .html(list[i].label)
+            .addClass("choice_label");
+        var li = $('<li/>')
+            .addClass("ui-state-default")
+            .append(img)
+            .append(caption);
+        $("#choicesList").append(li);
+    }
     $("#search-box").liveUpdate("#choicesList").focus();
 }
 
 $(document).ready(function() {
-    squarifyMe('#eventFrame', 0.75);
+    squarifyRatio('#eventFrame', 0.75);
     makeChoiceList(charities);
 
-    /*$("#choicesFrame").bind('mousewheel DOMMouseScroll', function(e){
-    	var scrollTo = null;
 
-    	if(e.type == 'mousewheel'){
-			scrollTo = (e.originalEvent.wheelDelta * -1);
-		}
-		else if(e.type == 'DOMMouseScroll'){
-			scrollTo = 40*e.originalEvent.detail;
-		}
+    var $currentlySelected = null;
+    var selected = [];
 
-		if(scrollTo) {
-			e.preventDefault();
-			$(this).scrollTop(scrollTo + $(this).scrollTop());
-		}
-    });*/
+    $('#choicesList').selectable({
+        start: function(event, ui){
+            $currentlySelected = $('#choicesList .ui-selected');
+        },
+        stop: function(event, ui){
+            for(var i = 0; i < selected.length; i++){
+                if($.inArray(selected[i],$currentlySelected) >= 0){
+                    $(selected[i]).removeClass('ui-selected');
+                }
+            }
+            selected = [];
+        },
+        selecting: function(event, ui) {
+            $currentlySelected.addClass('ui-selected');
+        },
+        selected: function(event, ui){
+            selected.push(ui.selected);
+        },
+        tolerance: 'fit'
+    });
 
-
-	var $currentlySelected = null;
-	var selected = [];
-
-	$('#choicesList').selectable({
-		start: function(event, ui){
-			$currentlySelected = $('#choicesList .ui-selected');
-		},
-		stop: function(event, ui){
-			for(var i = 0; i < selected.length; i++){
-				if($.inArray(selected[i],$currentlySelected) >= 0){
-					$(selected[i]).removeClass('ui-selected');
-				}
-			}
-			selected = [];
-		},
-		selecting: function(event, ui) {
-			$currentlySelected.addClass('ui-selected');
-		},
-		selected: function(event, ui){
-			selected.push(ui.selected);
-		},
-		tolerance: 'fit'
-	});
-
-	var highlightedButton = 0; // keeps track of which button is highlighted
+    var highlightedButton = 0; // keeps track of which button is highlighted
     var numButtons = 4;
+    //charities
     $("#btnFirst").click(function(){
         highlightedButton = 0;
         highlightButton(highlightedButton);
         $("#btnPrev").addClass("disabled");
-        //TODO implement function
+        changeChoicesList(highlightedButton);
     });
+    //event
     $("#btnSecond").click(function(){
         highlightedButton = 1;
         highlightButton(highlightedButton);
         $("#btnPrev").removeClass("disabled");
-        //TODO implement function
+        changeChoicesList(highlightedButton);
     });
+    //logistics
     $("#btnThird").click(function(){
         highlightedButton = 2;
         highlightButton(highlightedButton);
         $("#btnPrev").removeClass("disabled");
-        //TODO implement function
+        changeChoicesList(highlightedButton);
     });
+    //donations
     $("#btnFourth").click(function(){
         highlightedButton = 3;
         highlightButton(highlightedButton);
         $("#btnPrev").removeClass("disabled");
-        //TODO implement function
+        changeChoicesList(highlightedButton);
     });
     $("#btnPrev").click(function(){
         if (highlightedButton > 0){
@@ -114,27 +119,24 @@ $(document).ready(function() {
             highlightedButton = ((highlightedButton % numButtons) + numButtons) % numButtons;
             highlightButton(highlightedButton);
         }
-        //TODO implement functionality
+        changeChoicesList(highlightedButton);
     });
     $("#btnNext").click(function(){
         highlightedButton ++;
         highlightedButton = ((highlightedButton % numButtons) + numButtons) % numButtons;
         highlightButton(highlightedButton);
-        //TODO implement functionality
+        changeChoicesList(highlightedButton);
     });
 
     $("#btnPrev").addClass("disabled");
 });
 
-
-// highlilghts button indexed by number.  Also takes care of previous and next
+// highlights button indexed by number.  Also takes care of previous and next
 // disabling/text changing
 var highlightButton = function(num){
     var progressBar = $("#progressBar");
     var children = progressBar.children();
-    console.log(children.length);
     num = ((num % children.length) + children.length) % children.length;
-    console.log(num);
 
     // move progressindicator
     for (var i = 0; i < children.length; i ++){
@@ -159,3 +161,26 @@ var highlightButton = function(num){
         $("#btnNext").html("Next ►");
     }
 };
+
+// changes contents of choice list based on button number
+function changeChoicesList(btnNum){
+    if(btnNum === 0){
+        makeChoiceList(charities);
+        $("#choicesTitle").html("Choose your charity");
+        $("#choicesFrame").show();
+    }
+    else if(btnNum === 1){
+        makeChoiceList(event_types);
+        $("#choicesTitle").html("Choose your event types");
+        $("#choicesFrame").show();
+    }
+    else if(btnNum === 3){
+        makeChoiceList(donation_types);
+        $("#choicesTitle").html("Choose your donation types");
+        $("#choicesFrame").show();
+    }
+    else if(btnNum === 2){
+        $("#choicesFrame").hide();
+    }
+}
+
