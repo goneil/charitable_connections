6bbf6a3e2bdeb2e1e28b69a9cff40d0e3024ff5a
Gil O'Neil <goneil@mit.edu>
2013-04-17 19:55:06 -0400

    sizing on message for business suggestions

--- js/business-suggestions.js
+++ js/business-suggestions.js
@@ -6,37 +6,37 @@ $(document).ready(function() {
     var businessList = [
         new Business('Trader Joe\'s',
                      {location: "748 Memorial Dr Cambridge (617) 491-8582",
-                      info:"Trader Joes info",
+                      info:"Profiles specialty retail grocery store in nine states: California, Arizona, Nevada, Oregon, Washington, Massachusetts, New York, Connecticut, and New Jersey.",
                       donations: "Food Baskets"
                      }, './img/businesses/traderjoes.png'),
         new Business('Cambridge Bicycle',
                      {location: "259 Massachusetts Ave  Cambridge, MA 02139 (617) 876-6555",
-                      info:"Cambridge bike info",
+                      info:"DigThis Boston's Best Bike Shop. BMX, comfort bike, fixed gear, folding bike, hybrid bike, MTB, single speed, track bike.",
                       donations: "Bicycles"
                      }, './img/businesses/cambridgebicycle.png'),
         new Business('Qdoba',
                      {location: "1290 Massachusetts Ave  Cambridge, MA 02138 (617) 871-1136",
-                      info:"Qdoba info",
+                      info:"What will you find at your local Qdoba Mexican Grill? Food for people who love food.",
                       donations: "Food"
                      }, './img/businesses/qdoba.jpg'),
         new Business('Shaw\'s',
                      {location: "20 Sidney St Cambridge (617) 494-5250",
-                      info:"Shaws info",
+                      info:"Corporate overview of retail grocery chain, associate newsletter, career opportunities, promotions, weekly specials, and store locator.",
                       donations: "Food, Cookware, Beverages, Gift Certificates"
                      }, './img/businesses/shaws.gif'),
         new Business('Eastern Mountain Sports',
                      {locadtion: "1 Brattle Square #2  Cambridge, MA 02138 (617) 864-2061",
-                      info:"ems info",
+                      info:"Outdoor gear and equipment for sports and adventure.",
                       donations: "Gift Certificates"
                      }, './img/businesses/easternmountainsports.gif'),
         new Business('McDonald\'s',
                      {location: "463 Massachusetts Ave Cambridge (617) 497-3926",
-                      info:"McDonald's info",
+                      info:"McDonald's in the USA: Food and nutrition info, franchise opportunities, job and career info, restaurant locations, promotional information, history, innovation",
                       donations: "Food, Gift Certificates"
                      }, './img/businesses/mcdonalds.png'),
         new Business('City Sports',
                      {location: "   44 Brattle St  Cambridge, MA 02138 (617) 492-6000",
-                      info:"City Sports info",
+                      info:"City Sports offers the latest in athletic performance apparel, shoes, and accessories. Shop online to find top brand names and everything you need",
                       donations: "Gift Certificates"
                      }, './img/businesses/citysports.jpg')
     ];
@@ -77,25 +77,25 @@ $(document).ready(function() {
                         </br>\
                         <label class="control-label" for="theme">Theme</label>\
                         <div class="controls">\
-                          <input type="text" id="theme" placeholder="Theme">\
+                          <input type="text" id="theme" placeholder="Education">\
                         </div>\
                       </div>\
                       <div class="control-group">\
                         <label class="control-label" for="charity">Charity</label>\
                         <div class="controls">\
-                          <input type="text" id="charity" placeholder="Charity">\
+                          <input type="text" id="charity" placeholder="Jimmy Fund">\
                         </div>\
                       </div>\
                       <div class="control-group">\
                         <label class="control-label" for="date">Date</label>\
                         <div class="controls">\
-                          <input type="text" id="date" placeholder="xx/xx/xxxx">\
+                          <input type="text" id="date" placeholder="05/14/2013">\
                         </div>\
                       </div>\
                       <div class="control-group">\
                         <label class="control-label" for="size">Size</label>\
                         <div class="controls">\
-                          <input type="text" id="size" placeholder="Size">\
+                          <input type="text" id="size" placeholder="Medium">\
                         </div>\
                       </div>\
                       <div class="control-group">\
