9ea2cb77b3f52cfa6a03fa6147788d54e338ec1e
Max Stein-Golenbock <msteing@gmail.com>
2013-05-08 13:37:40 -0400

    added message and partial event retrieval from backend

--- js/my_events.js
+++ js/my_events.js
@@ -5,14 +5,51 @@ $(function() {
     var messageContainerID = "#msg-selectable";
     var eventContainerID = "#event-selectable";
     var eventFrameContainerID = "#event-frame-container";
-    //generateNewThread(messageContainerID,"Mockasins", "today", "eating");
-    generateNewThread(eventContainerID,"Mockasins", "today", "eating");
+    var visContainerID = "#vis-selectable";
+    var visFrameContainerID = "#vis-frame-container";
+    var hasMessage = false;
+
+    if (messages.length >= 1) {
+        hasMessage = true;
+        for (var i = 0; i < messages.length; i++) {
+            var message = messages[i];
+            var date = message.date;
+            if (date === undefined) {
+                date = "";
+            }
+            generateNewThread(messageContainerID, message.to, date, message.content.substring(0,40).trim()+"...");
+        }
+    } else {
+        alert("no messages");
+    }
+
+    var events = getEvents();
+    var hasEvents = false;
+    alert();
+
+    if (events.length >= 1) {
+        alert("true");
+        hasEvents = true;
+        for (var i = 0; i < events.length; i++) {
+            var newEvent = events[i];
+            generateNewThread(eventContainerID, newEvent.type, newEvent.date, newEvent.charities.substring(0,40).trim()+"...");
+        }
+    } else {
+        alert("no events");
+    }
+
+    
+    //generateNewThread(eventContainerID,"Ice Cream Scoop-athon", "May 25", "Dana Farber");
+    //generateNewThread(visContainerID,"Mockasins", "today", "eating");
+    //generateVisPane(visFrameContainerID);
+
+    
 
     $(messageContainerID).selectable({
         selected: function(event, ui) { 
             var thread = $(ui.selected);
-            //var messageID = parseInt(thread.attr("id").split("message")[1]);
-            //var content = messages[messageID].content;
+            var messageID = parseInt(thread.attr("id").split("message")[1]);
+            var content = messages[messageID].content;
             var date = $("#msg-date-label");
             var business = $("#msg-business-label");
             var eventName = $("#msg-event-label");
@@ -20,9 +57,8 @@ $(function() {
             $(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected");         
             date.html(thread.find(".thread-date").html());
             business.html(thread.find(".thread-business").html());
-            eventName.html(thread.find(".thread-event").html());
-            message_content.html(
-            "hello hola mi hola"
+            //eventName.html(thread.find(".thread-event").html());
+            message_content.html(content
             )
         }                   
     });
@@ -39,43 +75,15 @@ $(function() {
             business.html(thread.find(".thread-business").html());
             eventName.html(thread.find(".thread-event").html());
             generateEventPane(eventFrameContainerID);
-            //generateEventPane(containerID);
             //message_content.html(
             //"Hi blah yes"
             //)
         }                   
     });
 
-    /*
-    $("#vis-selectable").selectable({
-        selected: function(event, ui) { 
-            var thread = $(ui.selected);
-            var date = $("#vis-date-label");
-            var business = $("#vis-business-label");
-            var eventName = $("#vis-event-label");
-            var message_content = $("#vis-message-body");
-            $(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected");           
-            //addMessage("","","","");
-            // normally would get message based on message_id...  thread.attr("id");
-            date.html(thread.find(".thread-date").html());
-            business.html(thread.find(".thread-business").html());
-            eventName.html(thread.find(".thread-event").html());
-            )
-        }                   
-    });
-    */
 
  });   
                              
-/*
-function generateMessageThreads(messageList, containerID, business, date, eventName) {
-    for (int i = 0; i < messageList.length; i++) {
-        var message = messageList[i];
-        generateNewThread(containerID, message.to, message.from, eventName);
-    }
-}
-*/
-
 
 function generateNewThread(containerID, business, date, eventName) {
     var threadID = getNewThreadID(containerID);
@@ -97,6 +105,12 @@ function generateEventPane(containerID) {
     $(containerID).append(eventFrame);
 }
 
+function generateVisPane(containerID) {
+    clearContainer(containerID);
+    var visFrame = '<iframe src="business-wc.html"></iframe>';
+    $(containerID).append(visFrame);
+}
+
 function fillEventPane() {
 }
 
@@ -104,34 +118,17 @@ function clearContainer(containerID) {
     $(containerID).empty();
 }
 
-/*
- <li id="message1" class="ui-widget-content">
-                                    <div class="row-fluid">
-                                        <div class="span12 thread-business">
-                                            Charity connections word cloud
-                                        </div>
-                                    </div>
-                                    <div class="row-fluid thread-event-container">
-                                        <div class="span12 thread-event">
-                                            View the charities you've supported. 
-                                        </div>
-                                    </div>
-                                </li>
-
-                                <li id="message1" class="ui-widget-content">
-                                    <div class="row-fluid">
-                                        <div class="span12 thread-business">
-                                            Business connections word cloud
-                                        </div>
-                                    </div>
-                                    <div class="row-fluid thread-event-container">
-                                        <div class="span12 thread-event">
-                                            View the businesses you've contacted. 
-                                        </div>
-                                    </div>
-                                </li>
-*/
-
+var getEvents= function(){
+    var events;
+    $.ajax({
+    url: "./get_events",
+    success: function(data){
+        events = $.parseJSON(data);
+    },  
+    async: false
+    }); 
+    return events;
+};
 
 var getMessages = function(){
     var messages;
