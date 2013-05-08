8570d359b5db7630ccaa6d7e3601ea99be08cb60
Max Stein-Golenbock <msteing@gmail.com>
2013-05-08 00:03:04 -0400

    added my_events function in server.js to render user_id

--- js/my_events.js
+++ js/my_events.js
@@ -1,14 +1,16 @@
 $(function() {
     $( "#tabs" ).tabs();
+    addMessage("","","","");
     //$( "#selectable" ).selectable();
-    $("#selectable").selectable({
+    $("#msg-selectable").selectable({
         selected: function(event, ui) { 
             var thread = $(ui.selected);
-            var date = $("#date-label");
-            var business = $("#business-label");
-            var eventName = $("#event-label");
-            var message_content = $("#message-body");
+            var date = $("#msg-date-label");
+            var business = $("#msg-business-label");
+            var eventName = $("#msg-event-label");
+            var message_content = $("#msg-message-body");
             $(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected");           
+            //addMessage("","","","");
             // normally would get message based on message_id...  thread.attr("id");
             date.html(thread.find(".thread-date").html());
             business.html(thread.find(".thread-business").html());
@@ -18,21 +20,109 @@ $(function() {
             )
         }                   
     });
-    $(" .overlay-link[rel] ").overlay();
+
+    $("#event-selectable").selectable({
+        selected: function(event, ui) { 
+            var thread = $(ui.selected);
+            var date = $("#event-date-label");
+            var business = $("#event-business-label");
+            var eventName = $("#event-event-label");
+            var message_content = $("#event-message-body");
+            $(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected");           
+            //addMessage("","","","");
+            // normally would get message based on message_id...  thread.attr("id");
+            date.html(thread.find(".thread-date").html());
+            business.html(thread.find(".thread-business").html());
+            eventName.html(thread.find(".thread-event").html());
+            message_content.html(
+            "Hi blah yes"
+            )
+        }                   
+    });
+
+    $("#vis-selectable").selectable({
+        selected: function(event, ui) { 
+            var thread = $(ui.selected);
+            var date = $("#vis-date-label");
+            var business = $("#vis-business-label");
+            var eventName = $("#vis-event-label");
+            var message_content = $("#vis-message-body");
+            $(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected");           
+            //addMessage("","","","");
+            // normally would get message based on message_id...  thread.attr("id");
+            date.html(thread.find(".thread-date").html());
+            business.html(thread.find(".thread-business").html());
+            eventName.html(thread.find(".thread-event").html());
+            message_content.html(
+            "Hi Blah yes"
+            )
+        }                   
+    });
+
+    /*
+    $.get("./get_messages", {id: $.getUrlVar("_id")}, function(data){
+        data = $.parseJSON(data);
+        console.log(data);
+    }
+    */
+
+    //$(" .overlay-link[rel] ").overlay();
 });   
 
+/*
+var message_block = "<li id='message1' class='ui-widget-content'> \
+                                    <div class='row-fluid'> \
+                                        <div class='span10 thread-business'> \
+                                            The Jimmy Fund, Dana Farber \
+                                       </div> \
+                                        <div class="span2 thread-date"> \
+                                            Apr 30 \
+                                        </div> \
+                                    </div> \
+                                    <div class="row-fluid thread-event-container"> \
+                                        <div class="span12 thread-event"> \
+                                            Ice-Cream Scoopathon for Dana Farber \
+                                        </div> \
+                                    </div> \
+                                </li>";
+ 
+ */
+
+var $new_li = $('<li class="ui-widget-content"/>');
+var $new_row = $('div class="row-fluid"/>');
+var $new_thread_business = $('<div class="span10 thread-business" />');
+var $new_thread_date= $('<div class="span2 thread-date" />');
+var $new_thread_container = $('<div class="row-fluid thread-event-container" />');
+var $new_thread_event = $('<div class="span12 thread-event" />');
+
+                                    <div class="row-fluid">
+                                        <div class="span10 thread-business">
+                                            Trader Joe's
+                                        </div>
+                                        <div class="span2 thread-date">
+                                            Apr 30
+                                        </div>
+                                    </div>
+                                    <div class="row-fluid thread-event-container">
+                                        <div class="span12 thread-event">
+                                            Ice-Cream Scoopathon for Dana Farber
+                                        </div>
+                                    </div>
+
+
+
 function addMessage(business, date, eventName, message) {
-    $("#selectable").append(); // add info to thread selectable
-    //associate message_id in db with message body
+    $("#msg-selectable").append($new_li); // add info to thread selectable
+    $(new_li).append($new_row);
 }
 
+$.get("./get_event", {id: $.getUrlVar("event_id")}, function(data){
+    data = $.parseJSON(data);
+    console.log(data);
+});
 
-    
-    
-    
-    
-    
-    
+//function loadSelectable(messageLabel, dateLabel, businessLabl
+
     
 /**
     //$( document ).tooltip();
